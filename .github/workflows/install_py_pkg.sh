58287e57799c02ae5a1ee61da55436cc040e4844
air <airmanhigh@gmail.com>
2023-12-24 22:40:37 +0800

    ci&github: update the github workflow

--- .github/workflows/install_py_pkg.sh
+++ .github/workflows/install_py_pkg.sh
@@ -1,5 +1,6 @@
-cuda_version=$1
-gcc_version=$2
+source_root=$1
+cuda_version=$2
+gcc_version=$3
 
 conda create -n mink -y -c conda-forge 'python<3.11'
 
@@ -9,8 +10,4 @@ conda install -y -c pytorch -c nvidia pytorch "pytorch-cuda=$cuda_version"
 conda install -y -c anaconda openblas-devel
 conda install -y -c conda-forge "gcc=$gcc_version" gxx=$gcc_version"
 
-pip install -U git+https://github.com/NVIDIA/MinkowskiEngine -v \
-    --no-deps \
-    --config-setting="--install-option=--blas_include_dirs=${CONDA_PREFIX}/include" \
-    --config-setting="--install-option=--blas=openblas" \
-    --config-setting="--install-option=--force_cuda"
+cd $source_root && python setup.py install --blas_include_dirs=${CONDA_PREFIX}/include --blas=openblas --force_cuda
